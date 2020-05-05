5cb9607608ccbed08a1a6f8791f8da7596ff2be6
lromang <luis.roangarci@gmail.com>
2020-05-05 13:20:39 -0500

    signup working

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
+import AuthForm from "./AuthForm";
+import {graphql} from "react-apollo";
+import mutation from "../mutations/SignUp";
+import query from "../queries/CurrentUser";
+import {hashHistory} from "react-router";
 
 class SignUp extends Component {
     constructor(props) {
         super(props);
     }
 
+    onSubmit({email, password}) {
+        this.props.mutate({
+            variables: {email, password},
+            refetchQueries: [{query}]
+        }).then(() => {
+            hashHistory.push('/login')
+        })
+
+    }
+
     render() {
         return (
             <div>
-
+                <AuthForm onSubmit={this.onSubmit.bind(this)}/>
             </div>
         )
     }
 }
 
-export default SignUp;
+export default graphql(mutation)(graphql(query)(SignUp));
